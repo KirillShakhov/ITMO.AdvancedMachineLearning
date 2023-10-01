a9570d101bdff5d7d9fb8c2baaf762b345ba67a9
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-10-01 21:48:24 +0300

    feat(exercise1): task2

---
+++ exercise1/task2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Загрузите данные из файлов
+loadings = np.genfromtxt('X_loadings_441.csv', delimiter=';')
+reduced_data = np.genfromtxt('X_reduced_441.csv', delimiter=';')
+
+# Выберите первые десять загрузок
+first_ten_loadings = loadings[:, :10]
+
+# Восстановите исходное изображение
+restored_image = np.dot(reduced_data, first_ten_loadings.T)
+
+# Отобразите изображение
+plt.imshow(restored_image, cmap='gray')
+plt.title('Восстановленное изображение')
+plt.show()
